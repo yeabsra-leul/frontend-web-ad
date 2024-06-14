3ec234ac75410245a90df581adda273d314a2508
Cullen Lee <a410615903@gmail.com>
2024-06-14 01:16:45 +0000

    Add gitignore setting for /app/api; Take out api folder from the commit

--- app/api/create/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { createAdvertisement } from '@/lib/jsonHandler';
-
-export async function POST(req: NextRequest) {
-  const { adUrl, adLocation, adPhoneNumber, adChannel, adBudget, adHeadline1,adTargetAudience,adStartDate,adEndDate,adDescription,adSeoKeywords  } = await req.json();
-  const newAd = createAdvertisement(adUrl, adLocation, adPhoneNumber, adChannel, adBudget, adHeadline1,adTargetAudience,adStartDate,adEndDate,adDescription,adSeoKeywords );
-  return NextResponse.json(newAd);
-}
--- app/api/manage/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { createAdvertisement, getAds } from '@/lib/jsonHandler';
-
-export async function GET() {
-  const data = getAds();
-  return NextResponse.json(data);
-}
