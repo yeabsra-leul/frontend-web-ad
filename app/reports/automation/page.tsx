8e9b885002c53e5022c979c0e2c7a01c9ceaa7cc
prateek-pareek <97360950+prateek-pareek@users.noreply.github.com>
2024-06-28 22:30:58 +0530

    automation page added

---
+++ app/reports/automation/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import ScheduleAutomationPage from "@/components/ui/report/automation/automation"
+import React from "react";
+import Layout from "@/components/ui/layout/index";
+
+const Page: React.FC<any> = () => {
+    return (
+          <Layout>
+            <ScheduleAutomationPage/>
+          </Layout>
+    );
+  };
+  
+  
+   
+  export default Page
