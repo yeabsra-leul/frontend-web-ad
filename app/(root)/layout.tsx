123780d17b9c2f6f054be2268526bb74509737fd
Cullen Lee <a410615903@gmail.com>
2024-07-03 10:11:29 -0700

    chore: Update import paths for CSS files

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import '../globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SessionProvider } from 'next-auth/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
